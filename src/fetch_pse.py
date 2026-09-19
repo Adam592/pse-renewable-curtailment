2c7dc79430ec6e4b23bd43f82a866789578f2eb8
Adam592 <adam.jablonski010@gmail.com>
2026-09-19 20:21:17 +0200

    feat: add main logic for saving desired PSE data to a file

--- src/fetch_pse.py
+++ src/fetch_pse.py
@@ -1,24 +1,36 @@
 import requests
 import pandas as pd
 import logging
+from utils import save_raw, build_raw_filepath
 
 BASE_URL = "https://api.raporty.pse.pl/api/"
 DEFAULT_TIMEOUT = 30
-DEFAULT_BATCH_SIZE = 50000
+DEFAULT_BATCH_SIZE = 100000
+
+ENDPOINTS_CONFIG = {
+    "rce-pln": {
+        "start_date": "2024-07-01",
+        "end_date": "2026-09-01"
+    },
+    "poze-redoze": {
+        "start_date": "2024-10-01",
+        "end_date": "2026-09-01"
+    }
+}
 
 logger = logging.getLogger(__name__)
 
-def fetch_info(endpoint: str, start_date: str, select_params: str, *,
+def fetch_info(endpoint: str, start_date: str, *,
                 end_date: str | None=None, num_of_records: int = DEFAULT_BATCH_SIZE) -> pd.DataFrame:
     """The function fetches data from PSE API endpoint for a single day or a date range.
     Returns an empty DataFrame if fetch query failed."""
+
     if end_date is None:
         filter_str = f"business_date eq '{start_date}'"
     else:
         filter_str = f"business_date ge '{start_date}' and business_date le '{end_date}'"
 
     parameters = {
-        "$select": select_params,
         "$filter": filter_str,
         "$first": num_of_records
     }
@@ -38,6 +50,11 @@ def main() -> None:
     """Fetches poze-redoze and rce-pln in required scope.
     Saves the data to data/raw"""
 
+    for endpoint, config in ENDPOINTS_CONFIG.items():
+        result = fetch_info(endpoint=endpoint, start_date=config["start_date"], end_date=config["end_date"], num_of_records=DEFAULT_BATCH_SIZE)
+        filepath = build_raw_filepath(endpoint=endpoint, start_date=config["start_date"], end_date=config["end_date"])
+        save_raw(result, filepath)
+    
     return None
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import logging
 
 logger = logging.getLogger(__name__)
-
+RAW_DATA_DIR = Path("../data/raw")
 
 def save_raw(df :pd.DataFrame, filepath: Path) -> None:
     """The function given a dataframe saves it as a Parquet file to preselected filepath."""
@@ -17,3 +17,11 @@ def save_raw(df :pd.DataFrame, filepath: Path) -> None:
             logger.info(f"Successfully saved {len(df)} records to {filepath}")
     except Exception as e:
         logger.error(f"An error occurred saving the data: {e}")
+
+def build_raw_filepath(endpoint: str, start_date: str, *, end_date: str | None=None):
+    """Builds a filepath for raw data based on endpoint and dates given"""
+
+    if end_date is None:
+        return RAW_DATA_DIR / f"{endpoint}_{start_date}.parquet"
+    else:
+        return RAW_DATA_DIR / f"{endpoint}_{start_date}_to_{end_date}.parquet"
